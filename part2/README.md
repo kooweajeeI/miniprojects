cb6972abdb75fbed77dfa02d061e1d6d51c6719c
kooweajeeI <leejw2873@naver.com>
2023-05-02 14:01:54 +0900

    5월 2일

---
+++ part2/README.md
@@ -0,0 +1,11 @@
+# 미니프로젝트 Part2
+기간 - 2023.05.02 ~ 2023.05.16
+
+## WPF 학습
+- SCADA 시뮬레이션
+	- C# WPF
+	- MahApps.Metro (MetroUI 디자인 라이브러리)
+	- Bogus (더미데이터 생성 라이브러리)
+	- NewtonSoft.json
+	- M2Mqtt (통신 라이브러리)
+	
---
+++ part2/studySCADA/ScadaSimulation/BogusTestApp/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BogusTestApp.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal OrderValue { get; set; }
+        public bool Shipped { get; set; }
+
+    }
+}
