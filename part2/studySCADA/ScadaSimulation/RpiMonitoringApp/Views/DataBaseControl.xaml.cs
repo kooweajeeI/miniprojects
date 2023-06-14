1fe5821bc5f136afa26678779c53e6dabb7677b0
kooweajeeI <leejw2873@naver.com>
2023-06-14 17:23:36 +0900

    6월 14일
    라즈베리파이 센서모니터링

---
+++ part2/studySCADA/ScadaSimulation/RpiMonitoringApp/Views/DataBaseControl.xaml.cs
@@ -0,0 +1,205 @@
+﻿using MahApps.Metro.Controls;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using Org.BouncyCastle.Bcpg.OpenPgp;
+using SmartHomeMonitoringApp.Logics;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace SmartHomeMonitoringApp.Views
+{
+    /// <summary>
+    /// DataBaseControl.xaml에 대한 상호 작용 논리
+    /// </summary>
+
+
+    public partial class DataBaseControl : UserControl
+    {
+        public bool IsConnected { get; set; }
+
+        Thread MqttThread { get; set; }     // 없으면 UI컨트롤이 어려워짐
+
+        int MaxCount { get; set; } = 10;
+
+        // MQTT Subscribition text 과도문제 속도저하를 잡기위해 변수
+
+        public DataBaseControl()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            TxbBrokerUrl.Text = Commons.BROKERHOST;
+            TxbMqttTopic.Text = Commons.MQTTTOPIC;
+            TxtConnString.Text = Commons.MYSQL_CONNSTRING;
+
+            IsConnected = false;
+            BtnConnDb.IsChecked = false;
+
+            if (Commons.MQTT_CLIENT != null && Commons.MQTT_CLIENT.IsConnected)
+            {
+                IsConnected = true;
+                BtnConnDb.Content = "MQTT 연결중";
+                BtnConnDb.IsChecked = true;
+                Commons.MQTT_CLIENT.MqttMsgPublishReceived += MQTT_CLIENT_MqttMsgPublishReceived;
+            }
+        }
+
+        // 토글버튼 클릭 (1: 접속 2: 접속끊기) 이벤트 핸들러
+        private void BtnConnDb_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectDB();            
+        }
+
+        private void ConnectDB()
+        {
+            if (IsConnected == false)
+            {
+
+                // MQTT 브로커에 생성
+                Commons.MQTT_CLIENT = new uPLibrary.Networking.M2Mqtt.MqttClient(Commons.BROKERHOST);
+
+                try
+                {
+                    // MQTT 구독 로직처리
+                    if (Commons.MQTT_CLIENT.IsConnected == false)
+                    {
+
+                        // MQTT 접속
+                        Commons.MQTT_CLIENT.MqttMsgPublishReceived += MQTT_CLIENT_MqttMsgPublishReceived;
+                        Commons.MQTT_CLIENT.Connect("MONITOR"); // clientId = 모니터
+                        Commons.MQTT_CLIENT.Subscribe(new string[] { Commons.MQTTTOPIC },
+                                new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE }); // QOS는 네트워크 통신 옵션
+                        UpdateLog(">>> MQTT Broker Connected");
+
+                        BtnConnDb.IsChecked = true;
+                        IsConnected = true;
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    UpdateLog($"!!! MQTT Error 발생 : {ex.Message}");
+                }
+            }
+            else
+            {
+                try
+                {
+                    if (Commons.MQTT_CLIENT.IsConnected)
+                    {
+                        Commons.MQTT_CLIENT.MqttMsgPublishReceived -= MQTT_CLIENT_MqttMsgPublishReceived;
+                        Commons.MQTT_CLIENT.Disconnect();
+                        UpdateLog(">>> MQTT Broker Disconnrcted...");
+
+                        BtnConnDb.IsChecked = false;
+                        IsConnected = false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    UpdateLog($"!!! MQTT Error 발생 : {ex.Message}");
+                }
+            }
+        }
+
+        private void UpdateLog(string msg)
+        {
+            // 예외처리 필요
+            this.Invoke(() => {
+                if (MaxCount <= 0)
+                {
+                    TxtLog.Text = string.Empty;
+                    TxtLog.Text += ">>>문서 건수가 많아져 초기화!\n";
+                    TxtLog.ScrollToEnd();
+                    MaxCount = 10;
+                }
+                TxtLog.Text += $"{msg}\n";
+                TxtLog.ScrollToEnd();
+                MaxCount--;
+            });
+        }
+
+        // 구독이 발생할떄마다 실행되는 이벤트
+        private void MQTT_CLIENT_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            var msg = Encoding.UTF8.GetString(e.Message);
+            UpdateLog(msg);
+            SetToDataBase(msg, e.Topic);    // 실제 DB에 저장
+        }
+
+        // DB저장처리 메서드
+        private void SetToDataBase(string msg, string topic)
+        {
+            var currValue = JsonConvert.DeserializeObject<Dictionary<string, string>>(msg);
+            if (currValue != null)
+            {
+                Debug.WriteLine(currValue["DEV_ID"]);
+                Debug.WriteLine(currValue["TYPE"]);
+                Debug.WriteLine(currValue["CURR_DT"]);
+                Debug.WriteLine(currValue["STAT"]);
+
+                var tmp = currValue["STAT"].Split('|');
+                var temp = tmp[0].Trim();
+                var humid = tmp[1].Trim();
+
+                try
+                {
+                    using (MySqlConnection conn = new MySqlConnection(Commons.MYSQL_CONNSTRING))
+                    {
+                        if (conn.State == System.Data.ConnectionState.Closed) conn.Open();
+                        string insQuery = @"INSERT INTO smarthomesensor
+                                            (Home_Id,
+                                            Room_Name,
+                                            Sensing_DateTime,
+                                            Temp,
+                                            Humid)
+                                            VALUES
+                                            (@Home_Id,
+                                            @Room_Name,
+                                            @Sensing_DateTime,
+                                            @Temp,
+                                            @Humid)";
+
+                        MySqlCommand cmd = new MySqlCommand(insQuery, conn);
+                        cmd.Parameters.AddWithValue("@Home_Id", currValue["DEV_ID"]);
+                        cmd.Parameters.AddWithValue("@Room_Name", currValue["TYPE"]);
+                        cmd.Parameters.AddWithValue("@Sensing_DateTime", currValue["CURR_DT"]);
+                        cmd.Parameters.AddWithValue("@Temp", temp);
+                        cmd.Parameters.AddWithValue("@Humid", humid);
+
+                        if (cmd.ExecuteNonQuery() == 1)
+                        {
+                            UpdateLog(">>> DB Insert 성공");
+                        }
+                        else
+                        {
+                            UpdateLog(">>> DB Insert 실패");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    UpdateLog($"!!! Error 발생 : {ex.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ part2/studySCADA/ScadaSimulation/RpiMonitoringApp/Views/RealTimeControl.xaml.cs
@@ -0,0 +1,145 @@
+﻿using MahApps.Metro.Controls;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SmartHomeMonitoringApp.Logics;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace SmartHomeMonitoringApp.Views
+{
+    /// <summary>
+    /// RealTimeControl.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class RealTimeControl : UserControl
+    {
+        public RealTimeControl()
+        {
+            InitializeComponent();
+
+            LvcLivingTemp.Value = LvcDiningTemp.Value = LvcBedTemp.Value = LvcBathTemp.Value = 0;
+            LvcLivingHumid.Value = LvcDiningHumid.Value = LvcBedHumid.Value = LvcBathHumid.Value = 0;
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (Commons.MQTT_CLIENT != null && Commons.MQTT_CLIENT.IsConnected)
+            {   // DB 모니터링을 실행한 뒤 실시간 모니터링으로 넘어왔다면
+                Commons.MQTT_CLIENT.MqttMsgPublishReceived += MQTT_CLIENT_MqttMsgPublishReceived;
+            }
+            else
+            {   // DB 모니터링은 실행하지 않고 바로 실시간 모니터링 메뉴를 클릭했으면
+                Commons.MQTT_CLIENT = new MqttClient(Commons.BROKERHOST);
+                Commons.MQTT_CLIENT.MqttMsgPublishReceived += MQTT_CLIENT_MqttMsgPublishReceived;
+                Commons.MQTT_CLIENT.Connect("MONITOR");
+                Commons.MQTT_CLIENT.Subscribe(new string[] { Commons.MQTTTOPIC },
+                    new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+            }
+        }
+
+        // MQTTclient는 단독스레드 사용, UI스레드에 직접 접근이 안됨
+        // this.Invoke(); --> UI스레드 안에 있는 리소스 접근 가능
+
+        private void MQTT_CLIENT_MqttMsgPublishReceived(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs e)
+        {
+            var msg = Encoding.UTF8.GetString(e.Message);
+            Debug.WriteLine(msg);
+            var currSensor = JsonConvert.DeserializeObject<Dictionary<string, string>>(msg);
+
+            if (currSensor["DEV_ID"] == "IOT65")        
+            {
+                this.Invoke(() =>
+                {
+                    var dfValue = DateTime.Parse(currSensor["CURR_DT"]).ToString("yyyy-MM-dd HH:mm:ss");
+                    LblSensingDt.Content = $"Sensing DateTime : {dfValue}";
+                });
+                switch ("Living".ToUpper()) // CurrSensor["Room_Name"]을 안받기 때문에 Living으로 고정
+                {
+                    case "LIVING":
+                        this.Invoke(() =>
+                        {
+                            var tmp = currSensor["STAT"].Split('|');
+                            var temp = tmp[0].Trim();
+                            var humid = tmp[1].Trim();
+
+                            LvcLivingTemp.Value = Math.Round(Convert.ToDouble(temp), 1);
+                            LvcLivingHumid.Value = Convert.ToDouble(humid);
+                        });
+                        break;
+
+                    case "DINING":
+                        this.Invoke(() =>
+                        {
+                        LvcDiningTemp.Value = Math.Round(Convert.ToDouble(currSensor["Temp"]), 1);
+                        LvcDiningHumid.Value = Math.Round(Convert.ToDouble(currSensor["Humid"]), 1);
+                        });
+                        break;
+
+                    case "BED":
+                        this.Invoke(() =>
+                        {
+                        LvcBedTemp.Value = Math.Round(Convert.ToDouble(currSensor["Temp"]), 1);
+                        LvcBedHumid.Value = Math.Round(Convert.ToDouble(currSensor["Humid"]), 1);
+                        });
+                        break;
+
+                    case "BATH":
+                        this.Invoke(() =>
+                        {
+                            LvcBathTemp.Value = Math.Round(Convert.ToDouble(currSensor["Temp"]), 1);
+                        LvcBathHumid.Value = Math.Round(Convert.ToDouble(currSensor["Humid"]), 1);
+                        }); 
+                        break;
+
+                    default: 
+                        break;
+
+                }
+            }
+        }
+
+        private void BtnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            // Json으로 서보모터 90도 오픈한다는 데이터 생성
+            var topic = "pknu/monitor/control/";
+            
+            JObject origin_data = new JObject();
+            origin_data.Add("DEV_ID", "MONITOR");
+            origin_data.Add("CURR_DT", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+            origin_data.Add("STAT", "OPEN");
+            string pub_data = JsonConvert.SerializeObject(origin_data, Formatting.Indented);
+
+            Commons.MQTT_CLIENT.Publish(topic, Encoding.UTF8.GetBytes(pub_data));
+            LblDoorStat.Content = "OPEN!";
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            // Json으로 서보모터 90도 오픈한다는 데이터 생성
+            var topic = "pknu/monitor/control/";
+
+            JObject origin_data = new JObject();
+            origin_data.Add("DEV_ID", "MONITOR");
+            origin_data.Add("CURR_DT", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+            origin_data.Add("STAT", "CLOSE");
+            string pub_data = JsonConvert.SerializeObject(origin_data, Formatting.Indented);
+
+            Commons.MQTT_CLIENT.Publish(topic, Encoding.UTF8.GetBytes(pub_data));
+            LblDoorStat.Content = "CLOSE!!";
+        }
+    }
+}
